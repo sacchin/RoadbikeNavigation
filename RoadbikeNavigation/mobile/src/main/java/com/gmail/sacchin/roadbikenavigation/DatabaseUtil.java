38966041c85323def8bdc9981d393d0db282b827
satonaka yusuke <sacchin13@gmail.com>
2015-04-21 20:33:21 +0900

    データベースのUtilクラスを作成

---
+++ RoadbikeNavigation/mobile/src/main/java/com/gmail/sacchin/roadbikenavigation/DatabaseUtil.java
@@ -0,0 +1,23 @@
+package com.gmail.sacchin.roadbikenavigation;
+
+import android.database.sqlite.SQLiteException;
+
+import com.activeandroid.Model;
+import com.activeandroid.query.Select;
+
+/**
+ * Created by sacchin on 2015/04/21.
+ */
+public class DatabaseUtil{
+
+    public static int getMaxId(Class<? extends Model> sample ){
+        int id = 0;
+        try {
+            RouteModel route = new Select().from(sample).orderBy("id desc").executeSingle();
+            id = (route != null) ? route.id + 1 : 0;
+        }catch (SQLiteException e){
+            id = 0;
+        }
+        return id;
+    }
+}
